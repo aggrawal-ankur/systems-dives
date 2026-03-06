04d72a99d1252d639d086cbdb9c9349eb8c70cd5
hi-anki <managemee@protonmail.com>
2026-03-06 22:45:46 +0530

    bin structure has three configs, I am trying to get the x86 one right

--- main.py
+++ main.py
@@ -1,54 +1,35 @@
-print("Print x86 bin elements")
-for i in range(1, 64):
-  print(f'fd_{i*8}, bk_{i*8}')
+print("bins_x86[256] = {")
+print("// 1 unsorted bin + 62 small bins (total:63)")
+print("  fd_unsb, bk_unsb,")
+for i in range(2, 64):
+  print(f'  fd_{i*8}, bk_{i*8},')
 
 x = 512
+print("// 32 large bins of size 64 bytes")
 for i in range(1, 32+1):
-  print(f'fd_{x}, bk_{x}')
+  print(f'  fd_{x}, bk_{x},')
   x = x + 64
 
-x = 2560
+print("// 16 large bins of size 512 bytes")
 for i in range(1, 16+1):
-  print(f'fd_{x}, bk_{x}')
+  print(f'  fd_{x}, bk_{x},')
   x = x + 512
 
-x = 10752
+print("// 8 large bins of size 4096 bytes")
 for i in range(1, 8+1):
-  print(f'fd_{x}, bk_{x}')
+  print(f'  fd_{x}, bk_{x},')
   x = x + 4096
 
-x = 43520
+print("// 4 large bins of size 32768 bytes")
 for i in range(1, 4+1):
-  print(f'fd_{x}, bk_{x}')
+  print(f'  fd_{x}, bk_{x},')
   x = x + 32768
 
-x = 174592
+print("// 2 large bins of size 262144 bytes")
 for i in range(1, 2+1):
-  print(f'fd_{x}, bk_{x}')
+  print(f'  fd_{x}, bk_{x},')
   x = x + 262144
 
-
-print("Print x64 bin elements")
-for i in range(1, 64):
-  print(f'fd_{i*16}, bk_{i*16}')
-
-x = 1024
-for i in range(1, 32+1):
-  print(f'fd_{x}, bk_{x}')
-  x = x + 64
-
-for i in range(1, 16+1):
-  print(f'fd_{x}, bk_{x}')
-  x = x + 512
-
-for i in range(1, 8+1):
-  print(f'fd_{x}, bk_{x}')
-  x = x + 4096
-
-for i in range(1, 4+1):
-  print(f'fd_{x}, bk_{x}')
-  x = x + 32768
-
-for i in range(1, 2+1):
-  print(f'fd_{x}, bk_{x}')
-  x = x + 262144
+print("// 1 large bin of whatever size left")
+print(f'  fd_{x}, bk_{x},')
+print("}")
