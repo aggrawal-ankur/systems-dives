991fccd3aac5e7a8b33b3b8ae952eb8fd63a6513
hi-anki <managemee@protonmail.com>
2026-04-10 22:37:24 +0530

    Some double circular linked list implementations, written as a part of explaining the repositioning trick; there is one final one currently in process

---
+++ glibc-malloc/linked-list-code/2-array-dll.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node* next;
+  struct node* prev;
+};
+typedef struct node Node;
+
+struct list{
+  Node* head;
+  Node* tail;
+};
+typedef struct list List;
+
+Node* createNode(int initVal){
+  Node* n = malloc(sizeof(Node));
+  if (!n) return NULL;
+
+  n->data = initVal;
+  n->next = NULL;
+  n->prev = NULL;
+  return n;
+}
+
+int pushAtTail(List* lptr, int initVal){
+  Node *new_node = createNode(initVal);
+  if (!new_node){ return -1;}
+
+  /* Check empty list. */
+  if ((lptr->head == NULL) && (lptr->tail == NULL)){
+    lptr->head = new_node;
+    lptr->tail = new_node;
+    return 0;
+  }
+
+  /* List with one or more nodes. */
+
+  // Step1: Setup the new_node's links.
+  new_node->prev = lptr->tail;
+  new_node->next = lptr->head;
+
+  // Step2: Modify the tail node and the tail pointer.
+  (lptr->tail)->next = new_node;
+  lptr->tail = new_node;
+
+  // Step3: Modify the head node's prev link.
+  (lptr->head)->prev = lptr->tail;
+
+  return 0;
+}
+
+int pushAtHead(List* lptr, int initVal){
+  Node* new_node = createNode(initVal);
+  if (!new_node) return -1;
+
+  /* Check empty list. */
+  if ((lptr->head == NULL) && (lptr->tail == NULL)){
+    lptr->head = new_node;
+    lptr->tail = new_node;
+    return 0;
+  }
+
+  /* List with one or more nodes. */
+
+  // Step1: Set the new node's links.
+  new_node->next = lptr->head;
+  new_node->prev = lptr->tail;
+
+  // Step2: Modify the current lptr head and the head node.
+  (lptr->head)->prev = new_node;
+  lptr->head = new_node;
+
+  // Step3: Modify the tail node's next.
+  (lptr->tail)->next = new_node;
+}
+
+void displayFromHead(List* lptr){
+  Node* tmp = lptr->head;
+  int i = 0;
+  while (tmp->next != lptr->head){
+    printf("Node%d: %d\n", i, tmp->data);
+    i++;
+    tmp = tmp->next;
+  }
+  printf("Node%d: %d\n\n", i, tmp->data);
+}
+
+void displayFromTail(List* lptr){
+  Node* tmp = lptr->tail;
+  int i = 5;
+  while (tmp->prev != lptr->tail){
+    printf("Node%d: %d\n", i, tmp->data);
+    i--;
+    tmp = tmp->prev;
+  }
+  printf("Node%d: %d\n\n", i, tmp->data);
+}
+
+int deleteFromHead(List* lptr){
+  /* Empty list check. */
+  if (lptr->head == NULL){ return -1; }
+
+  /* Single node list. */
+  if (lptr->head == lptr->tail){
+    free(lptr->head);
+    lptr->head = NULL;
+    lptr->tail = NULL;
+    return 0;
+  }
+
+  /* List with more than one node. */
+  Node* cur_head = lptr->head;
+
+  // Step1: Modify the tail node's next.
+  (lptr->tail)->next = cur_head->next;
+
+  // Step2: Modify the second node's prev.
+  (cur_head->next)->prev = lptr->tail;
+
+  // Step3: Modify the list head ptr.
+  lptr->head = cur_head->next;
+
+  // Step4: free(cur_head)
+  free(cur_head);
+}
+
+int deleteFromTail(List* lptr){
+  /* Empty list check. */
+  if (lptr->head == NULL){ return -1; }
+
+  /* Single node list. */
+  if (lptr->head == lptr->tail){
+    free(lptr->tail);
+    lptr->head = NULL;
+    lptr->tail = NULL;
+    return 0;
+  }
+
+  /* List with more than one node. */
+  Node* cur_tail = lptr->tail;
+
+  // Step1: Modify the head node's prev.
+  (lptr->head)->prev = cur_tail->prev;
+
+  // Step2: Modify the second last node's next.
+  (cur_tail->prev)->next = lptr->head;
+
+  // Step3: Modify the list tail ptr.
+  lptr->tail = cur_tail->prev;
+
+  // Step4: free(cur_tail)
+  free(cur_tail);
+}
+
+int initLists(List* listArr, unsigned int count){
+  if (!listArr) return -1;
+  for (unsigned int i=0; i<count; i++){
+    listArr[i].head = NULL;
+    listArr[i].tail = NULL;
+  }
+  return 0;
+}
+
+int main(void){
+  List ten_lls[10] = {0};
+  initLists(&ten_lls[0], 10);
+
+  pushAtHead(&ten_lls[0], 5);
+  pushAtTail(&ten_lls[0], 6);
+  displayFromHead(&ten_lls[0]);
+}
---
+++ glibc-malloc/linked-list-code/3-nodeptr-array-impl.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int data;
+  struct node* next;
+  struct node* prev;
+};
+typedef struct node Node;
+
+Node* createNode(int initVal){
+  Node* n = malloc(sizeof(Node));
+  if (!n) return NULL;
+
+  n->data = initVal;
+  n->next = NULL;
+  n->prev = NULL;
+  return n;
+}
+
+int pushAtTail(Node** head, Node** tail, int initVal){
+  Node *new_node = createNode(initVal);
+  if (!new_node){ return -1;}
+
+  /* Check empty list. */
+  if ((*head == NULL) && (*tail == NULL)){
+    *head = new_node;
+    *tail = new_node;
+    return 0;
+  }
+
+  /* List with one or more nodes. */
+
+  // Step1: Setup the new_node's links.
+  new_node->prev = *tail;
+  new_node->next = *head;
+
+  // Step2: Modify the tail node and the tail pointer.
+  (*tail)->next = new_node;
+  *tail = new_node;
+
+  // Step3: Modify the head node's prev link.
+  (*head)->prev = *tail;
+
+  return 0;
+}
+
+int pushAtHead(Node** head, Node** tail, int initVal){
+  Node* new_node = createNode(initVal);
+  if (!new_node) return -1;
+
+  /* Check empty list. */
+  if ((*head == NULL) && (*tail == NULL)){
+    *head = new_node;
+    *tail = new_node;
+    return 0;
+  }
+
+  /* List with one or more nodes. */
+
+  // Step1: Set the new node's links.
+  new_node->next = *head;
+  new_node->prev = *tail;
+
+  // Step2: Modify the current lptr head and the head node.
+  (*head)->prev = new_node;
+  *head = new_node;
+
+  // Step3: Modify the tail node's next.
+  (*tail)->next = new_node;
+}
+
+void displayFromHead(Node* head){
+  Node* tmp = head;
+  int i = 0;
+  while (tmp->next != head){
+    printf("Node%d: %d\n", i, tmp->data);
+    i++;
+    tmp = tmp->next;
+  }
+  printf("Node%d: %d\n\n", i, tmp->data);
+}
+
+void displayFromTail(Node* tail){
+  Node* tmp = tail;
+  int i = 5;
+  while (tmp->prev != tail){
+    printf("Node%d: %d\n", i, tmp->data);
+    i--;
+    tmp = tmp->prev;
+  }
+  printf("Node%d: %d\n\n", i, tmp->data);
+}
+
+int deleteFromHead(Node* head, Node* tail){
+  /* Empty list check. */
+  if (head == NULL) return -1;
+
+  /* Single node list. */
+  if (head == tail){
+    free(head);
+    head = NULL;
+    tail = NULL;
+    return 0;
+  }
+
+  /* List with more than one node. */
+  Node* cur_head = head;
+
+  // Step1: Modify the tail node's next.
+  (tail)->next = cur_head->next;
+
+  // Step2: Modify the second node's prev.
+  (cur_head->next)->prev = tail;
+
+  // Step3: Modify the list head ptr.
+  head = cur_head->next;
+
+  // Step4: free(cur_head)
+  free(cur_head);
+}
+
+int deleteFromTail(Node* head, Node* tail){
+  /* Empty list check. */
+  if (head == NULL){ return -1; }
+
+  /* Single node list. */
+  if (head == tail){
+    free(tail);
+    head = NULL;
+    tail = NULL;
+    return 0;
+  }
+
+  /* List with more than one node. */
+  Node* cur_tail = tail;
+
+  // Step1: Modify the head node's prev.
+  (head)->prev = cur_tail->prev;
+
+  // Step2: Modify the second last node's next.
+  (cur_tail->prev)->next = head;
+
+  // Step3: Modify the list tail ptr.
+  tail = cur_tail->prev;
+
+  // Step4: free(cur_tail)
+  free(cur_tail);
+}
+
+int initListHeaders(Node** listHdrs, unsigned int listCount){
+  if (!listHdrs)  return -1;
+  for (unsigned int i=0; i<(listCount*2); i++){
+    listHdrs[i] = NULL;
+  }
+  return 0;
+}
+
+int main(void){
+  unsigned int listCount = 10;
+  Node* listHeaders[listCount*2];
+  initListHeaders(listHeaders, 10);
+
+  pushAtHead(&listHeaders[0], &listHeaders[1], 5);
+  pushAtTail(&listHeaders[0], &listHeaders[1], 6);
+  displayFromHead(listHeaders[0]);
+  displayFromTail(listHeaders[0]);
+}
